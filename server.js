561aeec375f18f33fc3647202e1e4e1e2cce5a7e
Joseph Kalb <kalb.joseph@gmail.com>
2019-07-22 11:17:56 -0700

    added live font adjustment

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const password = process.env.PASS || 'blbl'
 console.log(password)
 const port = process.env.PORT || 3000
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -18,15 +22,23 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-let value = '';
+let info = {
+    value:'testing',
+    font:50
+}
 app.post('/', (req, res) => {
-    const {query} = req
+    const {query, body} = req
+
+    console.log(body)
 
     if(query.password === password){
         console.log(query)
-        value = query.value
-        io.emit('value',value)
-        res.send(query)
+        info = {
+            value:body.value,
+            font:body.font
+        }
+        io.emit('value',info)
+        res.send(info)
     }
     else{
         res.json({
@@ -37,7 +49,7 @@ app.post('/', (req, res) => {
 })
 
 io.on('connection', socket => {
-    io.emit('value', value)
+    io.emit('value', info)
     console.log(`User connected`)
 })
 
